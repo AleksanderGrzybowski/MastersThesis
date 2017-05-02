75864da692cfb0b5201ebf808ca8a92a33809cfd
Aleksander Grzybowski <ifyouseethis@tellmeplease.it>
2017-05-02 21:15:05 +0200

    Use latex in docker, [T]

--- text/compile.sh
+++ text/compile.sh
@@ -1,7 +1,3 @@
 #! /bin/bash
 
-ssh root@192.168.8.2 'rm -rf /tmp/src'
-scp -r src root@192.168.8.2:/tmp/
-ssh root@192.168.8.2 'bash -c "cd /tmp/src && pdflatex content.tex content.pdf && pdflatex content.tex content.pdf"'
-scp -r root@192.168.8.2:/tmp/src/content.pdf .
-
+sudo docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD/src":/data blang/latex /bin/sh -c "pdflatex content.tex content.pdf && pdflatex content.tex content.pdf"
