28c77e23c431aeac98290157fb080dfffc7cf023
Aleksander Grzybowski <ifyouseethis@tellmeplease.it>
2017-05-17 19:21:27 +0200

    Tpc runner

--- plots/runall.sh
+++ plots/runall.sh
@@ -9,3 +9,12 @@ cd plots
 ./test.py ../build/reports/jmh/results.txt log "${name}"
 
 done
+
+for name in Tpc1Benchmark Tpc4Benchmark Tpc6Benchmark Tpc12Benchmark Tpc14Benchmark ; do
+
+cd ..
+./gradlew clean jmh -Pinclude="${name}.*"
+cd plots
+./test.py ../build/reports/jmh/results.txt lin "${name}"
+
+done
