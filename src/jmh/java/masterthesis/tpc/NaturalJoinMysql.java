b3ca7e314299020642c7940123a54d6874ba13cf
Aleksander Grzybowski <ifyouseethis@tellmeplease.it>
2017-05-22 20:08:51 +0200

    Trying something with joins

---
+++ src/jmh/java/masterthesis/tpc/NaturalJoinMysql.java
@@ -0,0 +1,51 @@
+package masterthesis.tpc;
+
+import mastersthesis.Utils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.openjdk.jmh.annotations.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import static mastersthesis.Utils.createSchemaMysql;
+
+@SuppressWarnings("SqlResolve")
+@State(Scope.Thread)
+public class NaturalJoinMysql {
+    
+    Connection connection;
+    
+    @Param({"0.01"})
+    public String scaleFactor;
+    
+    @Setup
+    public void setup() throws Exception {
+        connection = Utils.newDatabase("mysql");
+        createSchemaMysql(connection);
+    }
+    
+    @Benchmark
+    public List<ImmutablePair<Long, Long>> sql() throws Exception {
+        ResultSet rs = connection.prepareStatement(
+               "select\n" +
+                       "\tO_ORDERKEY, L_ORDERKEY\n" +
+                       "from\n" +
+                       "\tORDERS,\n" +
+                       "\tLINEITEM\n" +
+                       "where\n" +
+                       "\tO_ORDERKEY = L_ORDERKEY\n"
+        ).executeQuery();
+    
+        List<ImmutablePair<Long, Long>> result = new ArrayList<>();
+        
+        while (rs.next()) {
+            result.add(new ImmutablePair<>(rs.getLong(1), rs.getLong(2)));
+        }
+        
+        System.out.println("Mysql count(*) : " + result.size());
+        return result;
+    }
+}
+
---
+++ src/jmh/java/masterthesis/tpc/NaturalJoinStreamsHashmaps.java
@@ -0,0 +1,41 @@
+package masterthesis.tpc;
+
+import mastersthesis.Store;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SuppressWarnings("SqlResolve")
+@State(Scope.Thread)
+public class NaturalJoinStreamsHashmaps {
+    
+    Store store;
+    
+    
+    @Param({"0.01"})
+    public String scaleFactor;
+    
+    @Setup
+    public void setup() throws Exception {
+        store = new Store("dbgen");
+    }
+    
+    @Benchmark
+    public List<ImmutablePair<Long, Long>> sql() throws Exception {
+        
+        List<ImmutablePair<Long, Long>> result = StreamUtils.innerJoinHashmaps(
+                store.getOrders(),
+                o -> o.orderkey,
+                store.getLineItems(),
+                l -> l.order.orderkey
+        )
+                .map(pair -> new ImmutablePair<>(pair.left.orderkey, pair.right.order.orderkey))
+                .collect(Collectors.toList());
+        
+        System.out.println("Hashmaps streams count(*) : " + result.size());
+        return result;
+    }
+}
+
