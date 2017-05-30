a17f6881f18f604a2515060291f11baa19915ded
Aleksander Grzybowski <ifyouseethis@tellmeplease.it>
2017-05-30 21:25:44 +0200

    Tpc 14 hashmaps, [T]

---
+++ src/jmh/java/masterthesis/tpc/tpc14/Tpc14BenchmarkHashmaps.java
@@ -0,0 +1,85 @@
+package masterthesis.tpc.tpc14;
+
+import mastersthesis.BigDecimalSummaryStatistics;
+import mastersthesis.Store;
+import mastersthesis.Utils;
+import mastersthesis.model.LineItem;
+import mastersthesis.model.Part;
+import masterthesis.tpc.StreamUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.openjdk.jmh.annotations.*;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+/**
+ * Because of joins, SQL rocks here and streams are 50x slower.
+ */
+@SuppressWarnings("SqlResolve")
+@State(Scope.Thread)
+public class Tpc14BenchmarkHashmaps {
+    
+    Store store;
+    
+    @Param({"0.01", "0.02", "0.05", "0.1", "0.2"})
+    public String scaleFactor;
+    
+    @Setup
+    public void setup() throws Exception {
+        Utils.recreateData(scaleFactor);
+        store = new Store("dbgen");
+    }
+    
+    @Benchmark
+    public BigDecimal hashmaps() throws Exception {
+        Stream<ImmutablePair<Part, LineItem>> stream = StreamUtils.innerJoinHashmaps(
+                store.getParts(),
+                part -> part.partkey,
+                store.getLineItems(),
+                LineItem::getPartKey
+        );
+        
+        List<BigDecimalSummaryStatistics> list = stream.filter(pair -> (
+                pair.getRight().shipDate.compareTo(LocalDate.of(1995, 9, 1)) >= 0 &&
+                        pair.getRight().shipDate.compareTo(LocalDate.of(1995, 10, 1)) < 0
+        )).collect(
+                Collector.of(
+                        () -> Arrays.asList(
+                                new BigDecimalSummaryStatistics(),
+                                new BigDecimalSummaryStatistics()
+                        ),
+                        (List<BigDecimalSummaryStatistics> accumulator, Pair<Part, LineItem> newValue) -> {
+                            accumulator.get(0).accept(
+                                    newValue.getLeft().type.startsWith("PROMO") ? (
+                                            newValue.getRight().extendedPrice.multiply(BigDecimal.ONE.subtract
+                                                    (newValue.getRight().discount))
+                                    ) : BigDecimal.ZERO);
+                            accumulator.get(1).accept(
+                                    newValue.getRight().extendedPrice.multiply(BigDecimal.ONE.subtract
+                                            (newValue.getRight().discount))
+                            );
+                        },
+                        (r1, r2) -> {
+                            for (int i = 0; i < 2; i++) {
+                                r1.get(i).combine(r2.get(i));
+                            }
+                            return r1;
+                        }
+                )
+        );
+        BigDecimal result = new BigDecimal("100.00")
+                .multiply(list.get(0).getSum())
+                .divide(list.get(1).getSum(), RoundingMode.HALF_UP);
+    
+        System.out.println("J8S: " + result);
+        return result;
+    }
+    
+}
+
--- src/test/java/masterthesis/tpc/Tpc14Test.java
+++ src/test/java/masterthesis/tpc/Tpc14Test.java
@@ -1,6 +1,7 @@
 package masterthesis.tpc;
 
 import masterthesis.tpc.tpc14.Tpc14BenchmarkH2;
+import masterthesis.tpc.tpc14.Tpc14BenchmarkHashmaps;
 import masterthesis.tpc.tpc14.Tpc14BenchmarkMysql;
 import masterthesis.tpc.tpc14.Tpc14BenchmarkStreams;
 import org.junit.Test;
@@ -17,6 +18,10 @@ public class Tpc14Test {
         Tpc14BenchmarkStreams streams = new Tpc14BenchmarkStreams();
         streams.scaleFactor = "0.01";
         streams.setup();
+        
+        Tpc14BenchmarkHashmaps hashmaps = new Tpc14BenchmarkHashmaps();
+        hashmaps.scaleFactor = "0.01";
+        hashmaps.setup();
     
         Tpc14BenchmarkH2 h2 = new Tpc14BenchmarkH2();
         h2.scaleFactor = "0.01";
@@ -27,10 +32,12 @@ public class Tpc14Test {
         mysql.setup();
     
         BigDecimal resultStreams = streams.streams();
+        BigDecimal resultHashmaps = hashmaps.hashmaps();
         BigDecimal resultH2 = h2.sql();
         BigDecimal resultMysql = mysql.sql();
     
         assertEquals(resultStreams.setScale(5, RoundingMode.HALF_UP), resultMysql.setScale(5, RoundingMode.HALF_UP));
+        assertEquals(resultStreams.setScale(5, RoundingMode.HALF_UP), resultHashmaps.setScale(5, RoundingMode.HALF_UP));
         assertEquals(resultMysql.setScale(5, RoundingMode.HALF_UP), resultH2.setScale(5, RoundingMode.HALF_UP));
     }
 }
