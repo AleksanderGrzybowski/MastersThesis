3ef6871d90db5ac431a2f3706a64cdd8a0fb4810
Aleksander Grzybowski <ifyouseethis@tellmeplease.it>
2017-06-01 19:04:23 +0200

    [T]

--- src/main/java/mastersthesis/JavaMain.java
+++ src/main/java/mastersthesis/JavaMain.java
@@ -1,8 +1,13 @@
 package mastersthesis;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class JavaMain {
     
     public static void main(String[] args) throws Exception {
-        Utils.createSchemaMysql(Utils.newDatabase("mysql"));
+        System.out.println(
+                LocalDate.of(2017, Month.JANUARY, 31).plusMonths(1)
+        );
     }
 }
